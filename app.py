346fc9fe6010973d2de6042474ae5cfc803b3cbc
James Clark <J.Clark.NW@Gmail.com>
2021-05-09 11:30:31 -0700

    added ability for API to scrape table or ul

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask
 from flask_restful import Api, Resource, abort
 from bs4 import BeautifulSoup
 import requests
-
+import pandas
+import lxml
 
 
 app = Flask(__name__)
@@ -17,49 +18,71 @@ def check_link(page):
         print("Page loaded successfully")
 
 
-class ReturnIngredients(Resource):
+class ReturnTable(Resource):
+
+    def get(self, URL, table_num):
+
+            print("table scrape requested")
+            # Obtain wikipedia URL and designate CSV output
+            link = "https://en.wikipedia.org/wiki/" + URL
+
+            # Verify wikipedia link works, abort if not
+            page = requests.get(link)
+            check_link(page)
+
+            # Obtain tables into panda dataframe
+            dfs = pandas.read_html(link)
+            # Select specific table
+            table = dfs[table_num]
+            # Turn table into dictionary
+            table_output = table.to_dict()
+
+            return table_output
+
+
+class ReturnUL(Resource):
 
     def get(self, URL):
 
-        # create URL for Wikipedia page
-        link = "https://en.wikibooks.org/wiki/Cookbook:" + URL
+            print("ul scrape requested")
+            # create URL for Wikipedia page
+            link = "https://en.wikibooks.org/wiki/Cookbook:" + URL
 
-        # Verify wikipedia link works, abort if not
-        page = requests.get(link)
-        check_link(page)
+            # Verify wikipedia link works, abort if not
+            page = requests.get(link)
+            check_link(page)
 
-        # Obtain ingredients UL
-        soup = BeautifulSoup(page.content, 'html.parser')
-        body = soup.find("div", {"class": "mw-parser-output"})
-        ingredients = body.find_all("ul")
+            # Obtain ingredients UL
+            soup = BeautifulSoup(page.content, 'html.parser')
+            body = soup.find("div", {"class": "mw-parser-output"})
+            ingredients = body.find_all("ul")
 
-        # Create string with all ingredients seperated by a comma
-        for row in ingredients:
-            ingredients_list = ""
-            if row.get_text():
-                ingredients_list += row.get_text()
+            # Create string with all ingredients seperated by a comma
+            for row in ingredients:
+                ingredients_list = ""
+                if row.get_text():
+                    ingredients_list += row.get_text()
 
-        ingredients_list = ingredients_list.replace('\n', ',')
+            ingredients_list = ingredients_list.replace('\n', ',')
 
-        # Turn string into list
-        ingredients_list = ingredients_list.split(',')
+            # Turn string into list
+            ingredients_list = ingredients_list.split(',')
 
-        # return ingredient string
-        return ingredients_list
-
-
-class ReturnWorld(Resource):
-
-    def get(self):
-        return "Hello World"
+            # return ingredient string
+            return ingredients_list
 
 
 @app.route('/')
 def root():
-    return "This is the simple microservice"
+    return """This is the Table/UL Scraper microservice
+    To scrape a table use the input format of: /<string:URL>/<int:table_num>
+    To scrape a UL use the input format of: /<string:URL>
+    <string:URL> = end of wikipedia URL for desired page, ex: Kyle_Lewis for page on Seattle Mariner Kyle Lewis
+    <int:table_num> = index of table on page, ex: to return the first table on Kyle Lewis's page enter /Kyle_Lewis/0"""
 
 
-api.add_resource(ReturnIngredients, "/<string:URL>")
+api.add_resource(ReturnTable, "/<string:URL>/<int:table_num>")
+api.add_resource(ReturnUL, "/<string:URL>")
 
 
 if __name__ == "__main__":
